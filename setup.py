75f3bb2c5634112be14a4d4136eb95d01c6f5f3d
Meriem-Zouari <meriam.zouari@insat.ucar.tn>
2025-09-21 23:43:56 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from steuptools import setup, find_packages
+setup(
+    name='medical-chatbot',
+    version='0.1.0',
+    author='meriam zouari',
+    author_email='meriam.zouari@insat.ucar.tn',
+    description='A medical chatbot application',
+    packages=find_packages(),
+    install_requires=[]
+)
